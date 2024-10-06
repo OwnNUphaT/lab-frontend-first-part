1b4be4858d3bd6c20bf8fbe69dde1c169019bbc6
Ownny <120323931+OwnNUphaT@users.noreply.github.com>
2024-10-06 21:09:46 +0700

    json update

--- src/components/BaseInput.vue
+++ src/components/BaseInput.vue
@@ -1,17 +1,38 @@
 <script setup lang="ts">
+import { defineProps, withDefaults, defineEmits } from 'vue';
 
-const modelValue = defineModel()
-interface BaseInputProps {
-    label: string
+const props = withDefaults(defineProps<{
+    label: string;
+    modelValue?: string | number;
+}>(), {
+    label: '',
+    // eslint-disable-next-line vue/require-valid-default-prop
+    modelValue: {
+        type: [String, Number],
+        require: false,
+        default: ''
+    }
+});
+
+// Define emit
+const emit = defineEmits<{
+    (event: 'update:modelValue', value: string | number): void;
+}>();
+
+function updateValue(event: Event) {
+    emit('update:modelValue', (event.target as HTMLInputElement)?.value);
 }
-const props = withDefaults(defineProps<BaseInputProps>(), {
-    label: ''
-})
-
 </script>
+
 <template>
-    <label v-if="label">
+    <label v-if="props.label">
         {{ props.label }}
     </label>
-    <input class="mb-6" v-bind = "$attrs" v-model="modelValue" :placeholder="label" />
+    <input 
+        class="mb-6" 
+        :value="props.modelValue" 
+        :placeholder="props.label" 
+        @input="updateValue($event)" 
+        v-bind="$attrs" 
+    />
 </template>
